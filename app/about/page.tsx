1f648f9c047b8dc1a5bf7ad2d8b360457be7c070
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2025-05-18 01:27:58 +0200

    Update page title and navigation link to Swedish localization

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,7 +4,7 @@ import AuthorLayout from '@/layouts/AuthorLayout'
 import { coreContent } from 'pliny/utils/contentlayer'
 import { genPageMetadata } from 'app/seo'
 
-export const metadata = genPageMetadata({ title: 'About' })
+export const metadata = genPageMetadata({ title: 'Om oss' })
 
 export default function Page() {
   const author_1 = allAuthors.find((p) => p.slug === 'magnus') as Authors
--- data/headerNavLinks.ts
+++ data/headerNavLinks.ts
@@ -3,7 +3,7 @@ const headerNavLinks = [
   { href: '/blog', title: 'Blogg' },
   { href: '/tags', title: 'Tags' },
   { href: '/projects', title: 'Projects' },
-  { href: '/about', title: 'About' },
+  { href: '/about', title: 'Om oss' },
 ]
 
 export default headerNavLinks
