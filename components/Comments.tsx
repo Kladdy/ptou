bc1e6e8067631ed2c064c23e2fae1281e38815a2
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2025-05-18 15:16:56 +0200

    Add neccessary env variables, change color theme, autoload comments

--- components/Comments.tsx
+++ components/Comments.tsx
@@ -5,7 +5,7 @@ import { useState } from 'react'
 import siteMetadata from '@/data/siteMetadata'
 
 export default function Comments({ slug }: { slug: string }) {
-  const [loadComments, setLoadComments] = useState(false)
+  const [loadComments, setLoadComments] = useState(true)
   console.log(
     'wow...',
     process.env.NEXT_PUBLIC_GISCUS_CATEGORY_ID,
