4ff03314860c0481453866ae89c33ad2a8777b3e
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2025-10-02 19:02:44 +0200

    Update project with the link to the pod

--- data/projectsData.ts
+++ data/projectsData.ts
@@ -7,21 +7,20 @@ interface Project {
 
 const projectsData: Project[] = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Podden På tal om universum',
+    description:
+      'I avslappnade och lättsamma samtal med experter inom fysik-, teknik-, och naturvetenskapliga ämnet utforskar vi allt mellan himmel och jord - och långt utanför de gränserna.',
+    imgSrc: '/static/images/ptou_social_banner_v1.png',
+    href: 'https://rss.com/podcasts/ptou',
   },
-  {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blogg/the-time-machine',
-  },
+  // {
+  //   title: 'The Time Machine',
+  //   description: `Imagine being able to travel back in time or to the future. Simple turn the knob
+  //   to the desired date and press "Go". No more worrying about lost keys or
+  //   forgotten headphones with this simple yet affordable solution.`,
+  //   imgSrc: '/static/images/time-machine.jpg',
+  //   href: '/blogg/the-time-machine',
+  // },
 ]
 
 export default projectsData
