e816a6ddcb2440b79df0efdb86fe46514a4cc2a0
Sigfrid Stjärnholm <sigfrid@stjarnholm.com>
2026-01-23 20:36:49 +0100

    Crop images in fota atomer, publish post

---
+++ public/static/images/posts/siri-fota-atomer/figures/crop.sh
@@ -0,0 +1,8 @@
+magick light/Bild2.jpeg -crop 1040x590+20+20 light/Bild2_cropped.jpeg
+magick dark/Bild3.jpeg -crop 1040x590+20+20 dark/Bild3_cropped.jpeg
+
+magick light/Bild4.jpeg -crop 960x400+60+120 light/Bild4_cropped.jpeg
+magick dark/Bild5.jpeg -crop 960x400+60+120 dark/Bild5_cropped.jpeg    
+
+magick light/Bild6.jpeg -crop 960x490+80+80 light/Bild6_cropped.jpeg
+magick dark/Bild7.jpeg -crop 960x490+80+80 dark/Bild7_cropped.jpeg
